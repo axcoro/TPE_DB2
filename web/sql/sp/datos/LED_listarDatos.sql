2037aca43a19439073e96b130bdd973babd9e43c
axcoro <axcoro@gmail.com>
2013-10-13 17:39:53 -0300

    solo trae por estado

--- web/sql/sp/datos/LED_listarDatos.sql
+++ web/sql/sp/datos/LED_listarDatos.sql
@@ -19,13 +19,16 @@ BEGIN
 case  
 when _tipo = 0 then -- proveedores
     select p.id_proveedores, d.* from led_datos d
-	inner join led_proveedores p on d.id_datos = p.id_datos;
+	inner join led_proveedores p on d.id_datos = p.id_datos
+	where d.estado = 1;
 when _tipo = 1 then -- clientes
     select p.id_cliente, d.* from led_datos d
-	inner join led_clientes p on d.id_datos = p.id_datos;
+	inner join led_clientes p on d.id_datos = p.id_datos
+	where d.estado = 1;
 when _tipo = 2 then -- terceros
     select p.id_terceros, d.* from led_datos d
-	inner join led_terceros p on d.id_datos = p.id_datos;
+	inner join led_terceros p on d.id_datos = p.id_datos
+	where d.estado = 1;
 end case; 
 
 END$$
