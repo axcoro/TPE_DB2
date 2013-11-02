8ef5e39b2974ad5c34edc073936b4aa5506c5bce
Ax <axcoro@gmail.com>
2013-11-02 11:53:29 -0300

    actualizado el dumo

--- web/sql/dump.sql
+++ web/sql/dump.sql
@@ -808,10 +808,12 @@ t.precio_mano_obra,
 t.precio_articulos,
 t.precio_prestaciones,
 t.id_cliente,
-d.razon_social
+d.razon_social,
+d.estado
 from led_trabajos t
 inner join led_clientes c on c.id_cliente = t.id_cliente
-inner join led_datos d on d.id_datos = c.id_datos;
+inner join led_datos d on d.id_datos = c.id_datos
+order by d.estado desc, fecha_creacion;
 
 END//
 DELIMITER ;
